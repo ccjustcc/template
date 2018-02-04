7159a22b9e6dc928410f520e32e8a804034f1202
unknown <1303532426@qq.com>
2018-02-04 17:32:04 +0800

    production enviroment building

--- webpacktemplate/webpack.config.js
+++ webpacktemplate/webpack.config.js
@@ -1,17 +1,40 @@
+//words
+//manifest [ˈmanəˌfest]  n.清单
+//生产环境构建
+
+//开发工具
+//webpack's Watch Mode
+//这个实际就是命令行 编译
+//webpack-dev-server
+//这是个插件,有服务器功能同时还有watch功能
+//webpack-dev-middleware
+//一个中间件 配合express使用的,感兴趣访问  https://doc.webpack-china.org/guides/development/#-source-map
+
+//当生成带有hash的时候可以自动生成带相应js的Html文件
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+//清理生成文件夹中多余的文件,全清理掉再重新生成
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+//使用hmr(在webpack包中自带)
 const webpack = require('webpack');
+//用来压缩和删除没被引用的代码 tree shaking 为啥感觉没作用
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+
 module.exports = {
-    devtool: 'eval-source-map',
-    entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
+    devtool: 'eval-source-map',//sourcemap 用来定义文件 不同模式编译时间不同
+    entry:  {
+        main:__dirname + "/app/main.js",
+        print : __dirname + "/app/print.js"
+    },
     output: {
       path: __dirname + "/public",//打包后的文件存放的地方
-      filename: "bundle.js"//打包后输出文件的文件名
+      filename: '[name].bundle-[hash].js',//打包后输出文件的文件名
     },
     devServer: {
         contentBase: "./public",//本地服务器所加载的页面所在的目录
         historyApiFallback: true,//不跳转
         inline: true,//实时刷新,
-        hot:true
+        hot:true//hmr 热替换
       } ,
     module: {
         rules: [
@@ -46,6 +69,8 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
         }),
-        new webpack.HotModuleReplacementPlugin()//热加载插件
+        new webpack.HotModuleReplacementPlugin(),//热加载插件
+        new CleanWebpackPlugin(['public']),
+        new UglifyJSPlugin(),
     ],
   }
