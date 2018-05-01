3622fc9778e143a3ea82b8a82b9c8e244faf6aa5
unknown <1303532426@qq.com>
2018-05-01 10:42:38 +0800

    reset dev

--- gulptemplate/nodejs/event.js
+++
@@ -1,8 +0,0 @@
-var events = require('events');
-
-var myEvent =new  events.EventEmitter();
-
-myEvent.on('fuck',function(m){
-    console.log(m)
-})
-myEvent.emit('fuck','hellow mother fucker')
--- gulptemplate/nodejs/readFile.js
+++
@@ -1,5 +0,0 @@
-var fs = require('fs');
-
-var readMe = fs.readFileSync('Readme.txt','utf8');
-
-fs.writeFileSync('writeMe.txt',readMe)
